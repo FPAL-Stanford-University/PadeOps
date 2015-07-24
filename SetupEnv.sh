e000f1bdd2c733a48f9109c9a52e91b410e42b3e
Aditya Ghate <aditya90@stanford.edu>
2015-07-23 20:57:27 -0700

    Small changes to environment variables and CMakelists.txt

--- SetupEnv.sh
+++ SetupEnv.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 CWD=`pwd`
-
-export FC=ifort
+export COMPILER_ID=Intel
+export FC=mpif90
+export CC=mpicc
+export CXX=mpicxx
 export FFTW_PATH=`dirname ${CWD}`/fftw-3.3.4
 export DECOMP_PATH=`dirname ${CWD}`/2decomp_fft
