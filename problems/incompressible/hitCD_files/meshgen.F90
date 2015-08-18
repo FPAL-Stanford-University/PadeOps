41aa620f4e8b89c54a2b8f645c8f4a8be7795847
Aditya Ghate <aditya90@stanford.edu>
2015-08-18 14:04:44 -0700

    Fixed a minor bug in meshgen.F90

--- problems/incompressible/hitCD_files/meshgen.F90
+++ problems/incompressible/hitCD_files/meshgen.F90
@@ -1,15 +1,22 @@
-subroutine meshgen(nx,ny,nz,proc_st,proc_en,proc_sz,dx,dy,dz,mesh)
+subroutine meshgen(decomp, dx, dy, dz, mesh)
     use kind_parameters,  only: rkind
     use constants,        only: two,pi
+    use decomp_2d,        only: decomp_info
     implicit none
 
-    integer,                                                    intent(in)    :: nx,ny,nz
-    integer, dimension(3),                                      intent(in)    :: proc_st,proc_en,proc_sz
+    type(decomp_info),                                          intent(in)    :: decomp
     real(rkind),                                                intent(inout) :: dx,dy,dz
-    real(rkind), dimension(proc_sz(1),proc_sz(2),proc_sz(3),3), intent(inout) :: mesh
+    real(rkind), dimension(:,:,:,:), intent(inout) :: mesh
 
     integer :: i,j,k
+    integer :: nx, ny, nz, ix1, ixn, iy1, iyn, iz1, izn
 
+    nx = decomp%xsz(1); ny = decomp%ysz(2); nz = decomp%zsz(3)
+
+    ! If base decomposition is in Y
+    ix1 = decomp%yst(1); iy1 = decomp%yst(2); iz1 = decomp%yst(3)
+    ixn = decomp%yen(1); iyn = decomp%yen(2); izn = decomp%yen(3)
+    
     ! Create mesh from [0,2*pi)x[0,2*pi)x[0,2*pi) using nx, ny, nz points in x, y and z respectively
     ! Need to set x, y and z as well as  dx, dy and dz
 
@@ -19,9 +26,9 @@ subroutine meshgen(nx,ny,nz,proc_st,proc_en,proc_sz,dx,dy,dz,mesh)
         dy = two*pi/real(ny,rkind)
         dz = two*pi/real(nz,rkind)
 
-        do k=1,proc_sz(3)
-            do j=1,proc_sz(2)
-                do i=1,proc_sz(1)
+        do k=1,size(mesh,3)
+            do j=1,size(mesh,2)
+                do i=1,size(mesh,1)
                     x(i,j,k) = real( proc_st(1) + i - 1, rkind ) * dx
                     y(i,j,k) = real( proc_st(2) + j - 1, rkind ) * dy
                     z(i,j,k) = real( proc_st(3) + k - 1, rkind ) * dz
@@ -32,4 +39,3 @@ subroutine meshgen(nx,ny,nz,proc_st,proc_en,proc_sz,dx,dy,dz,mesh)
     end associate
 
 end subroutine
-
