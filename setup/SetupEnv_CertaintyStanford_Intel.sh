e407f8fc9dde19afdf42c3297b265888b89b316f
Aditya Ghate <aditya90@stanford.edu>
2016-01-10 16:22:57 -0800

    Merged everything and got it all to compile (on Certainty)

--- setup/SetupEnv_CertaintyStanford_Intel.sh
+++ setup/SetupEnv_CertaintyStanford_Intel.sh
@@ -7,3 +7,4 @@ export CC=mpicc
 export CXX=mpicxx
 export FFTW_PATH=${CWD}/dependencies/fftw-3.3.4
 export DECOMP_PATH=${CWD}/dependencies/2decomp_fft
+export VTK_IO_PATH=${CWD}/dependencies/Lib_VTK_IO/build
