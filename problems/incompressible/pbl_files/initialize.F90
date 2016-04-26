796240dc8140ea341a930007c7b307c35c7a0450
Aditya Ghate <aditya90@stanford.edu>
2016-04-26 00:21:30 -0500

    fixed the incorrect domain issue

--- problems/incompressible/pbl_files/initialize.F90
+++ problems/incompressible/pbl_files/initialize.F90
@@ -29,7 +29,7 @@ subroutine meshgen(decomp, dx, dy, dz, mesh)
     integer :: i,j,k
     integer :: ix1, ixn, iy1, iyn, iz1, izn
 
-    Lx = three; Ly = three; Lz = one
+    Lx = two*pi; Ly = two*pi; Lz = one
 
     nxg = decomp%xsz(1); nyg = decomp%ysz(2); nzg = decomp%zsz(3)
 
