6652c864c1a988b734b381dd5fea39b54f711c4f
Aditya Ghate <aditya90@stanford.edu>
2016-07-25 00:44:58 -0700

    Completed the 1d DCT implementation

--- src/utilities/dct_1d.F90
+++ src/utilities/dct_1d.F90
@@ -13,31 +13,44 @@ module dct_1d_stuff
         integer :: n, n1, n2
         integer(kind=8) :: plan_fwdDCT
         integer(kind=8) :: plan_bwdDCT
-
-        real(rkind) :: normfact, orthofactAll, orthofactFirst
+        integer(kind=8) :: plan_fwdDCT_ip 
+        integer(kind=8) :: plan_bwdDCT_ip
+        logical :: ortho = .false. 
+        real(rkind) :: normfact, orthofactAll, orthofactFirst, orthofactAll_inv, orthofactFirst_inv
 
         contains 
             procedure :: init
             procedure :: destroy
-            procedure :: dctx
-            procedure :: idctx
+            procedure, private :: dctx_oop
+            procedure, private :: idctx_oop
+            procedure, private :: dctx_ip
+            procedure, private :: idctx_ip
+            generic :: dctx => dctx_oop, dctx_ip
+            generic :: idctx => idctx_oop, idctx_ip
     end type
 
 contains
-    subroutine init(this, n, n1, n2)
+    subroutine init(this, n, n1, n2, ortho)
         class(dct1d), intent(inout) :: this
         integer, intent(in) :: n, n1, n2
         real(rkind), dimension(:), allocatable :: arr_in, arr_out
-         
+        logical :: ortho
+
         this%n = n; this%n2 = n2; this%n1 = n1
         this%normfact = one/(two*real(n,rkind))
-       
+        this%ortho = ortho
+
         allocate(arr_in(n), arr_out(n))
         call dfftw_plan_r2r_1d( this%plan_fwdDCT, n, arr_in, arr_out, FFTW_REDFT10, FFTW_EXHAUSTIVE )   
         call dfftw_plan_r2r_1d( this%plan_bwdDCT, n, arr_in, arr_out, FFTW_REDFT01, FFTW_EXHAUSTIVE )   
 
+        call dfftw_plan_r2r_1d( this%plan_fwdDCT_ip, n, arr_in, arr_in, FFTW_REDFT10, FFTW_EXHAUSTIVE )   
+        call dfftw_plan_r2r_1d( this%plan_bwdDCT_ip, n, arr_in, arr_in, FFTW_REDFT01, FFTW_EXHAUSTIVE )   
+        
         this%orthofactAll = sqrt(one/(two*real(n,rkind)))
         this%orthofactFirst = sqrt(one/two)
+        this%orthofactAll_inv = this%normfact*one/sqrt(one/(two*real(n,rkind)))
+        this%orthofactFirst_inv = one/sqrt(one/two)
     end subroutine
 
 
@@ -51,31 +64,110 @@ contains
 
     end subroutine
 
-
-    subroutine dctx(this, input, output)
+    subroutine dctx_oop(this, input, output)
         class( dct1d ), intent(in) :: this
-        real(rkind), dimension(this%n), intent(in) :: input
-        real(rkind), dimension(this%n), intent(out) :: output
+        real(rkind), dimension(this%n,this%n1,this%n2), intent(in) :: input
+        real(rkind), dimension(this%n,this%n1,this%n2), intent(out) :: output
+        integer :: j, k
 
-
-        call dfftw_execute_r2r( this%plan_fwdDCT, input, output )
-        output = this%orthoFactAll*output
-        output(1) = this%orthoFactFirst*output(1)
+        select case(this%ortho)
+        case(.true.)
+            do k = 1,this%n2
+                do j = 1,this%n1
+                    call dfftw_execute_r2r( this%plan_fwdDCT, input(:,j,k), output(:,j,k) )
+                    output(:,j,k) = this%orthoFactAll*output(:,j,k)
+                    output(1,j,k) = this%orthoFactFirst*output(1,j,k)
+                end do 
+            end do 
+        case(.false.)
+            do k = 1,this%n2
+                do j = 1,this%n1
+                    call dfftw_execute_r2r( this%plan_fwdDCT, input(:,j,k), output(:,j,k) )
+                end do 
+            end do 
+        end select
 
     end subroutine 
 
-    subroutine idctx(this, input, output)
+    subroutine idctx_oop(this, input, output)
         class( dct1d ), intent(in) :: this
-        real(rkind), dimension(this%n), intent(in) :: input
-        real(rkind), dimension(this%n), intent(out) :: output
+        real(rkind), dimension(this%n,this%n1,this%n2), intent(in) :: input
+        real(rkind), dimension(this%n,this%n1,this%n2), intent(out) :: output
         real(rkind), dimension(this%n) :: tmp
+        integer :: j, k
 
-        tmp = input/this%orthofactAll
-        tmp(1) = tmp(1)/this%orthoFactFirst
-        
-        call dfftw_execute_r2r( this%plan_bwdDCT, tmp, output )
-        
-        output = output*this%normfact
+
+        select case(this%ortho)
+        case(.true.)
+        do k = 1,this%n2
+            do j = 1,this%n1
+                tmp = input(:,j,k)*this%orthofactAll_inv
+                tmp(1) = tmp(1)*this%orthoFactFirst_inv
+                call dfftw_execute_r2r( this%plan_bwdDCT, tmp, output(:,j,k) )
+            end do 
+        end do 
+        case(.false.)
+            do k = 1,this%n2
+                do j = 1,this%n1
+                    tmp = input(:,j,k)*this%normfact
+                    call dfftw_execute_r2r( this%plan_bwdDCT, tmp, output(:,j,k) )
+                end do 
+            end do 
+            !output = this%normfact*output
+        end select
+
+    end subroutine
+
+    subroutine dctx_ip(this, input)
+        class( dct1d ), intent(in) :: this
+        real(rkind), dimension(this%n,this%n1,this%n2), intent(inout) :: input
+        integer :: j, k
+
+        select case(this%ortho)
+        case(.true.)
+            do k = 1,this%n2
+                do j = 1,this%n1
+                    call dfftw_execute_r2r( this%plan_fwdDCT, input(:,j,k), input(:,j,k) )
+                    input(1,j,k) = this%orthoFactFirst*input(1,j,k)
+                    input(:,j,k) = this%orthoFactAll*input(:,j,k)
+                end do 
+            end do 
+        case(.false.)
+            do k = 1,this%n2
+                do j = 1,this%n1
+                    call dfftw_execute_r2r( this%plan_fwdDCT, input(:,j,k), input(:,j,k) )
+                end do 
+            end do 
+        end select
 
     end subroutine 
+
+    subroutine idctx_ip(this, input)
+        class( dct1d ), intent(in) :: this
+        real(rkind), dimension(this%n,this%n1,this%n2), intent(inout) :: input
+        real(rkind), dimension(this%n) :: tmp
+        integer :: j, k
+
+
+        select case(this%ortho)
+        case(.true.)
+        do k = 1,this%n2
+            do j = 1,this%n1
+                input(:,j,k) = input(:,j,k)*this%orthofactAll_inv
+                input(1,j,k) = input(1,j,k)*this%orthoFactFirst_inv
+                call dfftw_execute_r2r( this%plan_bwdDCT, input(:,j,k), input(:,j,k) )
+            end do 
+        end do 
+        case(.false.)
+            do k = 1,this%n2
+                do j = 1,this%n1
+                    input(:,j,k) = input(:,j,k)*this%normfact
+                    call dfftw_execute_r2r( this%plan_bwdDCT, input(:,j,k), input(:,j,k) )
+                end do 
+            end do 
+            !input = this%normfact*input
+        end select
+
+    end subroutine
+
 end module 
--- tests/test_dct1d.F90
+++ tests/test_dct1d.F90
@@ -1,29 +1,98 @@
 program test_dct1d
     use kind_parameters, only: rkind
     use dct_1d_stuff, only: dct1d
-    
-    real(rkind), dimension(:), allocatable :: input, output
+    use timer, only: tic, toc
+    implicit none
+    real(rkind), dimension(:,:,:), allocatable :: input, output, inputn
+    complex(rkind), dimension(:,:,:), allocatable :: cinput, coutput, cinputn
+    logical :: useOrthogonalNorm = .false. 
+    integer(kind=8) :: c2cplanfwd, c2cplanbwd
+    real(rkind) :: normfact
     type(dct1d) :: mydct
-    integer :: n = 8
+    integer :: nx = 1024, ny = 32, nz = 32
     integer :: i 
+    include "fftw3.f"
+    
+    allocate(input(nx, ny, nz), output(nx, ny, nz), inputn(nx, ny, nz))
 
-    allocate(input(n), output(n))
-
-    do i = 1,n
-        input(i) = real(i,rkind)
+    do i = 1,nx
+        input(i,:,:) = real(i,rkind)
     end do 
     
-    call mydct%init(n,1,1)
+    call mydct%init(nx,ny,nz, useOrthogonalNorm)
 
+    print*, "TEST 1: Out-of-place DCTs" 
+    print*, "--------------------------"
+    call tic() 
     call mydct%dctx(input,output)
-    print*, output 
+    call toc()
+    call tic()
+    call mydct%idctx(output,inputn)
+    call toc()
+    print*, "Error:", maxval(abs(input - inputn))
+    print*, "=========================="
 
-    call mydct%idctx(output,input)
-    print*, input
-
+    inputn = input
+    print*, "TEST 2: In-place DCTs" 
+    print*, "--------------------------"
+    call tic()
+    call mydct%dctx(inputn)
+    call toc()
+    call tic()
+    call mydct%idctx(inputn)
+    call toc()
+    print*, "Error:", maxval(abs(input - inputn))
+    print*, "=========================="
 
     call mydct%destroy()
 
-    deallocate(input, output)
+    deallocate(input, output, inputn)
+
+    allocate(cinput(2*nx, ny, nz), coutput(2*nx,ny,nz), cinputn(2*nx,ny,nz))
+    
+    do i = 1,2*nx
+        cinput(i,:,:) = real(i,rkind)
+    end do 
+    normfact = 1.d0/(2.d0*real(nx,rkind))
+    call dfftw_plan_many_dft(c2cplanfwd, 1, 2*nx, ny*nz, cinput,2*nx, 1, 2*nx, &
+                            coutput, 2*nx, 1, 2*nx, FFTW_FORWARD, FFTW_EXHAUSTIVE)
+    call dfftw_plan_many_dft(c2cplanbwd, 1, 2*nx, ny*nz, cinput,2*nx, 1, 2*nx, &
+                            coutput, 2*nx, 1, 2*nx, FFTW_BACKWARD, FFTW_EXHAUSTIVE)
+
+    print*, "TEST 3: Out-of-place FFT with 2x extension" 
+    print*, "--------------------------"
+    call tic()
+    call dfftw_execute_dft( c2cplanfwd, cinput, coutput)  
+    call toc()
+    call tic()
+    call dfftw_execute_dft( c2cplanbwd, coutput, cinputn)
+    cinputn = cinputn*normfact
+    call toc()
+    print*, "Error:", maxval(abs(cinput - cinputn))
+    print*, "=========================="
+    call dfftw_destroy_plan ( c2cplanfwd )
+    call dfftw_destroy_plan ( c2cplanbwd )
+   
+    call dfftw_plan_many_dft(c2cplanfwd, 1, 2*nx, ny*nz, cinputn,2*nx, 1, 2*nx, &
+                            cinputn, 2*nx, 1, 2*nx, FFTW_FORWARD, FFTW_EXHAUSTIVE)
+    call dfftw_plan_many_dft(c2cplanbwd, 1, 2*nx, ny*nz, cinputn,2*nx, 1, 2*nx, &
+                            cinputn, 2*nx, 1, 2*nx, FFTW_BACKWARD, FFTW_EXHAUSTIVE)
+    cinputn = cinput
+        
+    print*, "TEST 4: In-place FFT with 2x extension" 
+    print*, "--------------------------"
+    call tic()
+    call dfftw_execute_dft( c2cplanfwd, cinputn, cinputn)
+    call toc()
+    call tic()
+    call dfftw_execute_dft( c2cplanbwd, cinputn, cinputn)
+    cinputn = cinputn*normfact
+    call toc()
+    print*, "Error:", maxval(abs(cinput - cinputn))
+    print*, "=========================="
+    call dfftw_destroy_plan ( c2cplanfwd )
+    call dfftw_destroy_plan ( c2cplanbwd )
+   
+    deallocate(cinput, cinputn, coutput)
 
 end program 
