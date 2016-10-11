8aeea1d788201bd9ec62c2ae740de9b397a8d6a4
Akshay Subramaniam <akshaysubr@gmail.com>
2016-10-11 13:51:26 -0700

    Updated setup script for Uzi with Intel compilers

--- setup/SetupEnv_UziStanford_Intel.sh
+++ setup/SetupEnv_UziStanford_Intel.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 export COMPILER_ID=Intel
-export FC=mpiifort
-export CC=mpiicc
-export CXX=mpiicpc
-export FFTW_PATH=/opt/fftw-3.3.4
+export FC=mpif90
+export CC=mpicc
+export CXX=mpicxx
+export FFTW_PATH=/opt/fftw-3.3.5-Intel
 export DECOMP_PATH=/opt/2decomp_fft
 export VTK_IO_PATH=/home/akshays/Codes/PadeOps/dependencies/Lib_VTK_IO/build
