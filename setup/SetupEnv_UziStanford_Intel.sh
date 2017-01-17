19e4e531345a42a5685beb106296988757734ccb
Akshay Subramaniam <akshaysubr@gmail.com>
2017-01-17 09:17:19 -0800

    Adding Intel setup script for Uzi

--- setup/SetupEnv_UziStanford_Intel.sh
+++ setup/SetupEnv_UziStanford_Intel.sh
@@ -7,3 +7,4 @@ export CXX=mpicxx
 export FFTW_PATH=/opt/fftw-3.3.4
 export DECOMP_PATH=/opt/2decomp_fft
 export VTK_IO_PATH=/home/akshays/Codes/PadeOps/dependencies/Lib_VTK_IO/build
+export HDF5_PATH=/opt/hdf5-intel-mvapich2
