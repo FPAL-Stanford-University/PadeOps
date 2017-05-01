ec8396dde0e9dd077618468d0231d03b5ebe1882
Akshay Subramaniam <akshaysubr@gmail.com>
2017-05-01 10:20:46 -0700

    Added compressible isotropic turbulence problem. Viscosity still needs to be added to this

---
+++ problems/compressible/CHIT.F90
@@ -0,0 +1,31 @@
+#include "CHIT_files/hooks.F90"
+
+program CHIT
+
+    use kind_parameters,  only: rkind,clen
+    use CompressibleGrid, only: cgrid
+    implicit none
+
+    type(cgrid) :: cgp
+    character(len=clen) :: inputfile
+    integer :: ierr 
+
+    ! Start MPI
+    call MPI_Init(ierr)
+
+    ! Get file location 
+    call GETARG(1,inputfile)
+    
+    ! Initialize the grid object
+    call cgp%init(inputfile)
+
+    ! Time advance
+    call cgp%simulate()
+        
+    ! Destroy everythin before ending
+    call cgp%destroy()
+
+    ! End the run
+    call MPI_Finalize(ierr)
+
+end program
