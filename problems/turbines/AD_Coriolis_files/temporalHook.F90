b6b7a797e96290e212698da4da3e0dd81a67515c
Aditya Ghate <aditya90@stanford.edu>
2017-07-01 13:52:48 -0600

    Updated the input file for AD_Coriolis

--- problems/turbines/AD_Coriolis_files/temporalHook.F90
+++ problems/turbines/AD_Coriolis_files/temporalHook.F90
@@ -9,7 +9,7 @@ module temporalHook
 
     implicit none 
 
-    integer :: nt_print2screen = 20
+    integer :: nt_print2screen = 1
     integer :: tid_statsDump = 5000
     integer :: tid_compStats = 100
     real(rkind) :: maxDiv, DomMaxDiv
