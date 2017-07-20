c28c36df91eab03613faa3a37e50084468472eb5
Aditya Ghate <aditya90@stanford.edu>
2017-07-20 15:57:24 -0600

    Added some skeleton code to do HIT forcing

--- src/incompressible/forcingIsotropic.F90
+++ src/incompressible/forcingIsotropic.F90
@@ -1,143 +1,84 @@
-module forcingMod
-    use kind_parameters, only: rkind
-    use decomp_2d, only: decomp_info, decomp_info_init, &
-                    transpose_x_to_y, transpose_y_to_x, &
-                    transpose_y_to_z, transpose_z_to_y 
-    use spectralMod, only: spectral 
-    use reductions, only: P_SUM
-    use exits, only: message 
-    use constants, only: zero
+module forcingmod
+   use kind_parameters, only: rkind
+   use decomp_2d, only: decomp_info
 
-    implicit none
+   implicit none
+   private
+   public :: HIT_shell_forcing
 
-    private
-    public :: forcing
+   type :: HIT_shell_forcing
+      private
+      type(decomp_info), pointer :: sp_gpC
+      real(rkind), dimension(:,:,:), allocatable :: k1, k2, k3, kshell
+      real(rkind) :: kmin, kmax
+      real(rkind) :: Nwaves
+      real(rkind) :: EpsAmplitude
 
-    real(rkind) :: dissConst = 0.1_rkind
-    type :: forcing
-        private
-        real(rkind) :: dissRate, KEforcing
-        integer, dimension(:), allocatable :: shell_i, shell_j, shell_k
-        integer :: nForceShellsG,nForceShellsL
-        real(rkind) :: multFact
-        real(rkind) :: nu
-        contains
-            procedure :: init
-            procedure :: addForcing
-            procedure :: destroy
-    end type  
+      real(rkind), dimension(:), allocatable :: wave_x, wave_y, wave_z
+      complex(rkind), dimension(:,:,:), allocatable :: uhat, vhat, what, fhat
+      contains
+      procedure          :: init
+      procedure          :: destroy
+      procedure, private :: pick_random_wavenumbers
+      procedure, private :: embed_forcing
+      procedure          :: getRHS_HITforcing
+   end type 
 
 contains
 
-    subroutine init(this,spect,kfmax,nxg,nyg,nzg,nu)
-        class(forcing), intent(inout) :: this
-        real(rkind), intent(in) :: kfmax
-        class(spectral), intent(in), target :: spect
-        real(rkind) :: kfmax_sq
-        real(rkind), dimension(:,:,:), pointer :: kabssq
-        integer :: counter, i, j, k 
-        integer, intent(in) :: nxg, nyg, nzg
-        real(rkind), intent(in) :: nu
+subroutine init(this, inputfile, sp_gp)
+   class(HIT_shell_forcing), intent(inout) :: this
+   character(len=*), intent(in) :: inputfile
+   type(decomp_info), intent(in), target :: sp_gp
+   integer :: nWaves = 20
+   real(rkind) :: kmin = 2.d0, kmax = 10.d0, EpsAmplitude = 0.1d0
+   namelist /HIT_Forcing/ kmin, kmax, Nwaves, EpsAmplitude 
 
-        counter = 0
-        kfmax_sq = kfmax**2
-        kabssq => spect%kabs_sq
-        do k = lbound(kabssq,3),ubound(kabssq,3)
-            do j = lbound(kabssq,2),ubound(kabssq,2)
-                do i = lbound(kabssq,1),ubound(kabssq,1)
-                    if (kabssq(i,j,k) .le. kfmax_sq) then
-                        counter = counter + 1
-                    end if 
-                end do 
-            end do 
-        end do 
+   open(unit=123, file=trim(inputfile), form='FORMATTED', iostat=ierr)
+   read(unit=123, NML=HIT_Forcing)
+   close(123)
 
-        allocate(this%shell_i(counter),this%shell_j(counter),this%shell_k(counter))
-        counter = 0
-        do k = lbound(kabssq,3),ubound(kabssq,3)
-            do j = lbound(kabssq,2),ubound(kabssq,2)
-                do i = lbound(kabssq,1),ubound(kabssq,1)
-                    if (kabssq(i,j,k) .le. kfmax_sq) then
-                        counter = counter + 1
-                        this%shell_i(counter) = i
-                        this%shell_j(counter) = j
-                        this%shell_k(counter) = k
-                    end if 
-                end do 
-            end do 
-        end do 
+   this%kmin = kmin
+   this%kmax = kmax
+   this%EpsAmplitude = EpsAmplitude
+   this%Nwaves = Nwaves
+   this%sp_gp => sp_gp
 
-        this%nu = nu
-        this%nForceShellsL = counter
-        this%nForceShellsG = P_SUM(this%nForceShellsL)
-        this%multFact = (real(nxg,rkind)*real(nyg,rkind)*real(nzg,rkind))**2
-        call message(0,"Initiazed the FORCING derived type successfully")
-        call message(1,"Total number of forced shells (global)", this%nForceShellsG)
-    end subroutine
+end subroutine 
 
-    subroutine addForcing(this,Sfield,duidxj,rhs)
-        class(forcing), intent(in) :: this
-        complex(rkind), dimension(:,:,:,:), intent(in), target :: Sfield
-        complex(rkind), dimension(:,:,:,:), intent(inout)      :: rhs
-        real(rkind) :: Elocal, Eglobal
-        integer :: shellid, i, j, k
-        real(rkind), dimension(:,:,:,:), intent(in), target :: duidxj
-        complex(rkind), dimension(:,:,:), pointer :: uhat, vhat, what
-        real(rkind) :: mconst
-        real(rkind), dimension(:,:,:), pointer :: dudy, dudz, dvdx, dvdz, dwdx, dwdy
-        real(rkind) :: enstrophy, dissipation
 
-        uhat => Sfield(:,:,:,1)
-        vhat => Sfield(:,:,:,1)
-        what => Sfield(:,:,:,1)
-        dudy => duidxj(:,:,:,2); dudz => duidxj(:,:,:,3)
-        dvdx => duidxj(:,:,:,4); dvdz => duidxj(:,:,:,6)
-        dwdx => duidxj(:,:,:,7); dwdy => duidxj(:,:,:,8)
-        
-        Elocal = zero
-        do shellid = 1,this%nForceShellsL
-            i = this%shell_i(shellid) 
-            j = this%shell_j(shellid) 
-            k = this%shell_k(shellid) 
+subroutine destroy(this)
+   class(HIT_shell_forcing), intent(inout) :: this
 
-            Elocal = Elocal + real(uhat(i,j,k),rkind)**2 + aimag(uhat(i,j,k))**2
-            Elocal = Elocal + real(vhat(i,j,k),rkind)**2 + aimag(vhat(i,j,k))**2
-            Elocal = Elocal + real(what(i,j,k),rkind)**2 + aimag(what(i,j,k))**2
-        end do 
-        Eglobal = P_SUM(Elocal)
-        
-        enstrophy = zero
-        do k = lbound(dudy,3),ubound(dudy,3)
-            do j = lbound(dudy,2),ubound(dudy,2)
-                do i = lbound(dudy,1),ubound(dudy,1)
-                    enstrophy = enstrophy + (dwdy(i,j,k) - dvdz(i,j,k))**2 + & 
-                        (dudz(i,j,k) - dwdx(i,j,k))**2 + (dvdx(i,j,k) - dudy(i,j,k))**2
-                end do 
-            end do 
-        end do 
-        enstrophy = enstrophy/sqrt(this%multFact)
-        dissipation = this%nu*P_SUM(enstrophy)
-        mconst = dissConst*dissipation*this%multFact/Eglobal
-        call message(0,"Mult Const:", mconst)
-       
-        do shellid = 1,this%nForceShellsL
-            i = this%shell_i(shellid) 
-            j = this%shell_j(shellid) 
-            k = this%shell_k(shellid) 
-            
-            rhs(i,j,k,1) = rhs(i,j,k,1) + mconst*uhat(i,j,k)
-            rhs(i,j,k,2) = rhs(i,j,k,2) + mconst*vhat(i,j,k)
-            rhs(i,j,k,3) = rhs(i,j,k,3) + mconst*what(i,j,k)
-        end do
+   nullify(this%sp_gpC)
+end subroutine 
 
-    end subroutine
+end module
 
-    subroutine destroy(this)
-        class(forcing), intent(inout) :: this
-    
-        if (allocated(this%shell_i)) deallocate(this%shell_i)
-        if (allocated(this%shell_j)) deallocate(this%shell_j)
-        if (allocated(this%shell_k)) deallocate(this%shell_k)
 
-    end subroutine
+
+subroutine getRHS_HITforcing(this, urhs_xy, vrhs_xy, wrhs_xy, uhat_xy, vhat_xy, what_xy)
+   class(HIT_shell_forcing), intent(inout) :: this
+   complex(rkind), dimension(:,:,:), intent(in) :: uhat_xy, vhat_xy, what_xy
+   complex(rkind), dimension(:,:,:), intent(inout) :: urhs_xy, vrhs_xy, wrhs_xy
+   
+   integer :: idx
+
+
+
+   ! STEP 1: Populate wave_x, wave_y, wave_z
+
+
+   ! STEP 2: embed into fhat
+   call this%embed_forcing()
+   ! transform from fhat -> fhat_xy
+   ! then add to urhs
+
+
+
+end subroutine
+
+
+
+
 end module 
