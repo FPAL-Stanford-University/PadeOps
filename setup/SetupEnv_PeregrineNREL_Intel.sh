81b2a30fa3589d4d9a6f8d99e8ea078f9e27f2a3
Aditya Ghate <aditya90@stanford.edu>
2017-08-14 15:14:29 -0700

    Removed the fftpack dependency last check before merge into igridSGS

--- setup/SetupEnv_PeregrineNREL_Intel.sh
+++ setup/SetupEnv_PeregrineNREL_Intel.sh
@@ -9,4 +9,3 @@ export FFTW_PATH=${CWD}/dependencies/fftw-3.3.5
 export DECOMP_PATH=${CWD}/dependencies/2decomp_fft
 export VTK_IO_PATH=${CWD}/dependencies/Lib_VTK_IO/build
 export HDF5_PATH=/${CWD}/dependencies/hdf5-1.8.18
-export FFTPACK_PATH=${CWD}/dependencies/fftpack
