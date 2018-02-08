c0bbad971e5b449462434045d42e37e02a465b47
Aditya Ghate <aditya90@stanford.edu>
2018-02-07 22:22:47 -0600

    Updated the HIT problem and cleaned it up a bit

--- problems/incompressible/HIT_Periodic_moving_files/temporalHook.F90
+++ problems/incompressible/HIT_Periodic_moving_files/temporalHook.F90
@@ -11,14 +11,8 @@ module temporalHook
     implicit none 
 
     integer :: nt_print2screen = 1
-    integer :: tid_statsDump = 5000
-    integer :: tid_compStats = 100
-    real(rkind) :: time_startDumping = 10.0_rkind, maxDiv, DomMaxDiv
+    real(rkind) :: DomMaxDiv
     integer :: ierr 
-    
-    integer :: tid_start_planes = 1
-    integer :: tid_stop_planes = 100000
-    integer :: tid_dump_plane_every = 10000
 
 contains
 
@@ -30,23 +24,6 @@ contains
          y => gp%mesh(:,:,:,2)
          x => gp%mesh(:,:,:,1)
    
-        ! case (1)
-        !    uexact  =  sin(x)*cos(y)*exp(-(2.d0/gp%Re)*gp%tsim)
-        !    vexact  = -cos(x)*sin(y)*exp(-(2.d0/gp%Re)*gp%tsim)
-        !    wexact  = 0.d0
-        !    pexact  = 0.25d0*(cos(2.d0*x) + cos(2.d0*y))*((exp(-(2.d0/gp%Re)*gp%tsim))**2)
-        ! case (2)
-        !    uexact  =  sin(x)*cos(z)*exp(-(2.d0/gp%Re)*gp%tsim)
-        !    vexact  = 0.d0
-        !    wexact  = -cos(x)*sin(z)*exp(-(2.d0/gp%Re)*gp%tsim)
-        !    pexact  = 0.25d0*(cos(2.d0*x) + cos(2.d0*z))*((exp(-(2.d0/gp%Re)*gp%tsim))**2)
-        ! case (3)
-        !    uexact  = 0.d0
-        !    vexact  =  sin(y)*cos(z)*exp(-(2.d0/gp%Re)*gp%tsim)
-        !    wexact  = -cos(y)*sin(z)*exp(-(2.d0/gp%Re)*gp%tsim)
-        !    pexact  = 0.25d0*(cos(2.d0*y) + cos(2.d0*z))*((exp(-(2.d0/gp%Re)*gp%tsim))**2)
-        ! end select
-       
          x_shift = x_shift + uadvect*gp%get_dt()
 
          if (mod(gp%step,gp%t_dataDump) == 0) then
@@ -64,22 +41,16 @@ contains
          end if 
         
          if (mod(gp%step,nt_print2screen) == 0) then
-            maxDiv = maxval(gp%divergence)
-            DomMaxDiv = p_maxval(maxDiv)
+            DomMaxDiv = p_maxval(gp%divergence)
             call message(0,"Time",gp%tsim)
             call message(1,"TIDX:",gp%step)
             call message(1,"MaxDiv:",DomMaxDiv)
-            call message(1,"Mean TKE:",gp%getMeanKE())
             call message_min_max(1,"Bounds for u:", p_minval(minval(gp%u)), p_maxval(maxval(gp%u)))
             call message_min_max(1,"Bounds for v:", p_minval(minval(gp%v)), p_maxval(maxval(gp%v)))
             call message_min_max(1,"Bounds for w:", p_minval(minval(gp%w)), p_maxval(maxval(gp%w)))
             if (allocated(gp%pressure)) then
                call message_min_max(1,"Bounds for P:", p_minval(minval(gp%pressure)), p_maxval(maxval(gp%pressure)))
             end if 
-            !call message(1,"Max Error u:",p_maxval(maxval(abs(gp%u  - uexact))))
-            !call message(1,"Max Error v:",p_maxval(maxval(abs(gp%v  - vexact))))
-            !call message(1,"Max Error w:",p_maxval(maxval(abs(gp%wC - wexact))))
-            !call message(1,"Max Error P:",p_maxval(maxval(abs(gp%pressure - pexact))))
             if (gp%useCFL) then
                 call message(1,"Current dt:",gp%dt)
             end if 
@@ -88,6 +59,5 @@ contains
         end if 
 
     end subroutine
-
 
 end module 
