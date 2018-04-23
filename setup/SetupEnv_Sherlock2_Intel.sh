ea9a4eb34d4ace34513379ae9cac5f17e0de08e7
Aditya Ghate <aditya90@stanford.edu>
2018-04-23 15:37:12 -0700

    Updated the setup file for sherlock

--- setup/SetupEnv_Sherlock2_Intel.sh
+++ setup/SetupEnv_Sherlock2_Intel.sh
@@ -9,3 +9,4 @@ export FFTW_PATH=${CWD}/dependencies/sherlock2/fftw-3.3.5
 export DECOMP_PATH=${CWD}/dependencies/sherlock2/2decomp_fft
 export VTK_IO_PATH=${CWD}/dependencies/sherlock2/Lib_VTK_IO/build
 export HDF5_PATH=${CWD}/dependencies/sherlock2/hdf5-1.8.18
+export ARCH_OPT_FLAG='-xCORE-AVX2'
