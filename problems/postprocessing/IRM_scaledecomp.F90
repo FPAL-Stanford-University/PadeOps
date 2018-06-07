327b858f133e8bc104a0a3fc621340365ff11ccd
Akshay Subramaniam <6964110+akshaysubr@users.noreply.github.com>
2018-06-07 14:16:02 -0700

    Cleaned up some of the problem related stuff in scale decomposition code

--- problems/postprocessing/IRM_scaledecomp.F90
+++ problems/postprocessing/IRM_scaledecomp.F90
@@ -32,24 +32,6 @@ module IRM_scaledecomp_mod
     integer               :: XY_COMM
     integer               :: xyrank, xyproc
 
-    ! This part is from the Miranda problem file to recalculate the physical species diffusivity
-    ! Material properties list ------ Property --------  Air ------------ CO2 --------
-    real(rkind), DIMENSION(2) :: mySig    = (/ 3.681D0            , 3.952D0   /)
-    real(rkind), DIMENSION(2) :: myTeff   = (/ 91.46D0            , 200.0D0   /)
-    real(rkind), DIMENSION(2) :: myMolwts = (/ 28.013D0           , 44.010D0  /)
-    real(rkind), DIMENSION(2) :: myGammas = (/ 1.4D0              , 1.28D0    /)
-    real(rkind), DIMENSION(2) :: myPr     = (/ 0.72D0             , 0.77D0    /)
-    real(rkind), DIMENSION(2) :: myMix    = (/ 1.0D0              , 1.0D0     /)
-    real(rkind), DIMENSION(2) :: myNuA    = (/ 1.45D-5            , 1.59D-5   /) ! in g/(cm-s-K^0.5)
-    real(rkind), DIMENSION(2) :: myNuB    = (/ 1.105D2            , 2.438D2   /) ! in K
-    real(rkind), DIMENSION(2) :: myNuC    = (/ 0.0D0              , 0.0D0     /) ! in g/(cm-s)
-    real(rkind), DIMENSION(2) :: myNuN    = (/ 1.5D0              , 1.5D0     /)
-
-    real(rkind), PARAMETER :: Runiv  = 8.314472D+7 ! Gas constant              [g*cm^2/(s^2*mol*K)]=[erg/K/mol]
-
-    real(rkind), PARAMETER :: Cd = real(3.0D-2,rkind)
-    real(rkind), PARAMETER :: CY = real(1.0D2 ,rkind)
-
 contains
 
     subroutine read_inputs(inputfile)
@@ -75,251 +57,6 @@ contains
         z_bc = [z_bc1, z_bcn]
 
     end subroutine
-
-    subroutine get_pdf(input,nbins,bins,min_input,max_input,pdf,mask)
-        use reductions, only: P_MAXVAL, P_MINVAL, P_SUM
-
-        real(rkind), dimension(:,:,:), intent(in) :: input
-        integer, intent(in) :: nbins
-        real(rkind), intent(in) :: max_input, min_input
-        real(rkind), dimension(:,:,:), intent(in) :: mask  ! This should only have zeros and ones
-        real(rkind), dimension(nbins+1), intent(out) :: bins
-        real(rkind), dimension(nbins), intent(out) :: pdf
-
-        real(rkind), dimension(nbins) :: proc_count
-        real(rkind) ::binsize
-
-        integer :: i, j, k, nb, ierr
-
-        ! Create equispaced bins between maximum and minimum
-        binsize = (max_input - min_input)/real(nbins,rkind)
-        bins(1) = min_input
-        do nb = 2,nbins+1
-            bins(nb) = bins(nb-1) + binsize
-        end do
-
-        proc_count = zero
-        ! Get the counts per bin for this proc
-        do k=1,mir%gp%ysz(3)
-            do j=1,mir%gp%ysz(2)
-                do i=1,mir%gp%ysz(1)
-                    if ( mask(i,j,k) .LT. half ) cycle ! No need to go to the bin loop if mask is zero
-                    do nb=1,nbins
-                        if ( ( input(i,j,k) .GE. bins(nb) ) .AND. ( input(i,j,k) .LE. bins(nb+1) ) ) then
-                            proc_count(nb) = proc_count(nb) + one
-                            exit ! No need to go over the other bins
-                        end if
-                    end do
-                end do
-            end do
-        end do
-
-        call MPI_ALLREDUCE(proc_count, pdf, nbins, mpirkind, MPI_SUM, MPI_COMM_WORLD, ierr) ! Now all procs will have the total counts per bin
-
-        ! Now normalize the pdf to make sum of probabilities equal to unity
-        pdf = pdf / (binsize*sum(pdf))
-
-    end subroutine
-
-    subroutine write_pdf(step, varname, bins, pdf)
-        integer, intent(in) :: step
-        character(len=*), intent(in) :: varname
-        real(rkind), dimension(:), intent(in) :: bins, pdf
-
-        character(len=clen) :: filename
-        integer :: i,n,iounit=51
-
-        ! Assume all processors have same pdf array, so only master proc can
-        ! write out the file
-        n = size(pdf)
-
-        write(filename,'(4A,I4.4,A)') trim(outputdir), "/pdf_", trim(varname), "_", step, ".dat"
-        call message(1,"Writing " // trim(varname) // " pdf to " // trim(filename))
-        if (nrank == 0) then
-            open(unit=iounit, file=trim(filename), form='FORMATTED', status="REPLACE")
-
-            write(iounit,'(2A)') "# pdf for ", trim(varname)
-            write(iounit,'(2A26)') "# bin", "PDF"
-            do i=1,n
-                write(iounit,'(2ES26.16)') half*(bins(i)+bins(i+1)), pdf(i)
-            end do
-
-            close(iounit)
-        end if
-
-    end subroutine
-
-    subroutine get_volumefractions(Y,X)
-        real(rkind), dimension(mir%gp%ysz(1), mir%gp%ysz(2), mir%gp%ysz(3), mir%ns), intent(in)  :: Y    ! Species mass fractions
-        real(rkind), dimension(mir%gp%ysz(1), mir%gp%ysz(2), mir%gp%ysz(3), mir%ns), intent(out) :: X    ! Species volume fractions
-        
-        real(rkind), dimension(mir%gp%ysz(1), mir%gp%ysz(2), mir%gp%ysz(3)) :: M
-        
-        integer :: n
-
-        ! Effective Molecular weight for EOS
-        M = zero
-        DO n = 1,mir%ns
-           M = M + Y(:,:,:,n)/myMolwts(n)
-        END DO
-        M = one/M
-        
-        ! Get volume fractons
-        do n = 1,mir%ns
-            X(:,:,:,n) = Y(:,:,:,n)*M/myMolwts(n)
-        end do
-   
-    end subroutine
-
-! ------------------------------------------------------------------------------
-! Assign problem-specific viscosity, thermal conductivity,
-! species diffusivities and magnetic diffusivity.
-! ------------------------------------------------------------------------------ 
-    SUBROUTINE prob_properties(mu,ktc,Diff)
-     USE constants, ONLY : half,three,two
-
-     real(rkind), dimension(mir%gp%ysz(1), mir%gp%ysz(2), mir%gp%ysz(3)),                   intent(out) :: mu,ktc    ! Shear visc and conductivity
-     real(rkind), dimension(mir%gp%ysz(1), mir%gp%ysz(2), mir%gp%ysz(3), mir%ns),           intent(out) :: Diff      ! Species diffusivity
-
-     real(rkind), dimension(:,:,:),   pointer :: T,rho,p
-     real(rkind), dimension(:,:,:,:), pointer :: Y
-     
-     real(rkind), DIMENSION(mir%gp%ysz(1), mir%gp%ysz(2), mir%gp%ysz(3)) :: M,CpMix,Rgas,Ts,Omega
-     real(rkind), DIMENSION(mir%gp%ysz(1), mir%gp%ysz(2), mir%gp%ysz(3)) :: mu_i,tmp,dum,Dij,Xii,Xjj
-     real(rkind) :: Acc,Bcc,Ccc,Dcc,Ecc,Fcc,Gcc,Hcc
-     real(rkind) :: Mij,sigij,Teij
-     INTEGER :: n,ni,nj
-  
-     ! Point T, Y, rho and p to mir variables so we can use Miranda code here directly
-     T   => mir%T
-     Y   => mir%Ys
-     rho => mir%rho
-     p   => mir%p
-
-     ! Effective Molecular weight for EOS
-     M = zero
-     DO n = 1,mir%ns
-        M = M + Y(:,:,:,n)/myMolwts(n)
-     END DO
-     M = one/M
-     Rgas = Runiv/M
-   
-   
-     ! Chapman-Cowling Viscosity
-     mu = zero
-     ktc = zero
-     tmp = zero
-     Acc = 1.16145D0
-     Bcc = -0.14874D0
-     Ccc = 0.52487D0
-     Dcc = -0.7732D0
-     Ecc = 2.16178D0
-     Fcc = -2.43787D0
-     DO n=1,mir%ns
-         Ts = T/myTeff(n)
-         Omega = Acc*Ts**Bcc + Ccc*exp(Dcc*Ts) + Ecc*exp(Fcc*Ts)     ! Edit: Ecc** -> Ecc*
-         mu_i = 2.6693D-6*sqrt( myMolwts(n)*T)/(Omega*mySig(n)**2)
-         dum = Y(:,:,:,n)/(myMolwts(n)**half)
-         mu = mu + mu_i * dum
-         tmp = tmp + dum
-   
-        ! Thermal diff part
-         CpMix = myGammas(n)*Y(:,:,:,n)/(myMolwts(n)*(myGammas(n)-one))*Runiv
-         ktc = ktc + CpMix*mu_i*dum/myPr(n)
-   
-     END DO
-     mu = mu/tmp * 10.0D0  ! convert kg/m s to g/cm s
-     ktc = ktc/tmp
-   
-     ! Diffusivity
-     Diff = zero
-     tmp = zero
-     Acc = 1.06036D0
-     Bcc = -0.1561D0
-     Ccc = 0.19300D0
-     Dcc = -0.47635D0
-     Ecc = 1.03587D0
-     Fcc = -1.52996D0
-     Gcc = 1.76474D0
-     Hcc = -3.89411D0
-     DO ni=1,mir%ns
-         Xii = Y(:,:,:,ni)*M/myMolwts(ni)
-         tmp = zero
-         DO nj=1,mir%ns
-            IF (ni .NE. nj) THEN
-               Xjj = Y(:,:,:,nj)*M/myMolwts(nj)
-               Mij = two/(one/myMolwts(ni)+one/myMolwts(nj))
-               sigij = (mySig(ni)+mySig(nj))/two
-               Teij = sqrt( myTeff(ni)*myTeff(nj) )
-               Ts = T/Teij                                         ! Edit: Add omega calculation
-               Omega = Acc*Ts**Bcc + Ccc*exp(Dcc*Ts) + Ecc*exp(Fcc*Ts) + Gcc*exp(Hcc*Ts)
-               Dij = 0.0266/Omega * T**(three/two)/(p*sqrt(Mij)*sigij**2 )
-               tmp = tmp + Xjj/Dij
-            END IF
-         END DO
-         Diff(:,:,:,ni) = (one-Xii)/(tmp + 1.0D-16)
-     END DO
-   
-     Diff = Diff * 10.0D4 / 10.0D0 ! Convert m^2/s to cm^2/s  (extra /10 is from pressure)
-   
-    END SUBROUTINE prob_properties
-
-    SUBROUTINE sgs_diffusivity(Y,Diff)
-     real(rkind), DIMENSION(:,:,:), INTENT(IN) :: Y
-     real(rkind), DIMENSION(:,:,:), INTENT(OUT) :: Diff
-     real(rkind), DIMENSION(SIZE(Y,1),SIZE(Y,2),SIZE(Y,3)) :: Lart,Lden,art,tmp,dum,Dsgs,Ysgs
-
-     real(rkind), dimension(mir%gp%xsz(1),mir%gp%xsz(2),mir%gp%xsz(3)) :: xbuf1, xbuf2
-     real(rkind), dimension(mir%gp%zsz(1),mir%gp%zsz(2),mir%gp%zsz(3)) :: zbuf1, zbuf2
-
-     Diff = zero
-   ! Oscillation control (Dsgs)========================================================================
-        Lart = SQRT(mir%dx**2 + mir%dy**2 + mir%dz**2) ! grid scale
-   ! Kawai's formula ----------------------------------
-          dum = one
-          call transpose_y_to_x( Y, xbuf1, mir%gp )
-          CALL der%d2dx2(xbuf1,xbuf2)
-          CALL der%d2dx2(xbuf2,xbuf1)
-          call transpose_x_to_y( xbuf1, tmp, mir%gp )
-          Dsgs = dum**4 * tmp * mir%dx**5
-          Lart = ABS(tmp)*mir%dx     ! Numerator for Y weighted grid length
-          Lden = tmp**2              ! Denominator for Y weighted grid length
-   
-          dum = one
-          CALL der%d2dy2(Y,art)
-          CALL der%d2dy2(art,tmp)
-          Dsgs = Dsgs + dum**4 * tmp * mir%dy**5
-          Lart = Lart + ABS(tmp)*mir%dy
-          Lden = Lden + tmp**2
-   
-          dum = one
-          call transpose_y_to_z( Y, zbuf1, mir%gp )
-          CALL der%d2dz2(zbuf1,zbuf2)
-          CALL der%d2dz2(zbuf2,zbuf1)
-          call transpose_z_to_y( zbuf1, tmp, mir%gp )
-          Dsgs = Dsgs + dum**4 * tmp * mir%dz**5
-          Lart = Lart + ABS(tmp)*mir%dz
-          Lden = Lden + tmp**2
-   
-          Dsgs = Cd*mir%c*Dsgs
-          Lart = Lart/( SQRT(Lden) + eps)
-   ! Kawai's formula ----------------------------------
-   
-   ! Bounds control (Ysgs)=============================================================================
-          Ysgs = CY*mir%c*0.5*(ABS(Y)  -one+ABS(one-Y  ))*Lart   !    0<=Y<=1
-   ! ceiling + filter==================================================================================
-        art = MAX(Dsgs,Ysgs)
-        tmp = art
-        ! CALL filter(gfilter,tmp,art) 
-        call transpose_y_to_x( tmp, xbuf1, mir%gp )
-        call gfil%filterx(xbuf1,xbuf2)
-        call transpose_x_to_y( xbuf2, art, mir%gp )
-        call gfil%filtery( art, tmp )
-        call transpose_y_to_z( tmp, zbuf1, mir%gp )
-        call gfil%filterz(zbuf1,zbuf2)
-        call transpose_z_to_y( zbuf2, art, mir%gp )
-        Diff = MAX(Diff,art)                   ! cm^2/s
-    END SUBROUTINE sgs_diffusivity
 
     subroutine get_duidxj(u,v,w,duidxj)
         use operators, only: gradient
@@ -581,9 +318,6 @@ program IRM_scaledecomp
 
     real(rkind) :: KE_L_int, KE_S_int, production_int, baropycnal_int, pdil_L_int, pdil_S_int, diss_L_int, diss_S_int
 
-    integer :: nbins = 64
-    real(rkind), dimension(:), allocatable :: bins, pdf
-
     logical :: vizstep = .false.
 
     character(len=clen) :: time_message
@@ -657,9 +391,6 @@ program IRM_scaledecomp
     diss_L      => buffer(:,:,:,i);            i = i+1
     diss_S      => buffer(:,:,:,i);            i = i+1
 
-    allocate( bins(nbins+1) )
-    allocate( pdf (nbins  ) )
-    
     allocate( duidxj_ff(mir%gp%ysz(1), mir%gp%ysz(2), mir%gp%ysz(3), 9) )
     allocate( tauij    (mir%gp%ysz(1), mir%gp%ysz(2), mir%gp%ysz(3), 6) )
 
@@ -696,9 +427,6 @@ program IRM_scaledecomp
 
         ! Read in data for this viz dump
         call mir%read_data(step)
-
-        ! Get the physical mu, ktc and Diff
-        call prob_properties(mu,ktc,Diff)
         
         ! Get filtered density, velocities, pressure
         call filter(gfil, mir%rho, rho_f, x_bc, y_bc, z_bc)
@@ -783,8 +511,6 @@ program IRM_scaledecomp
 
     ! Destroy all variables and exit cleanly
     if (allocated( buffer ))    deallocate( buffer   )
-    if (allocated( bins   ))    deallocate( bins     )
-    if (allocated( pdf    ))    deallocate( pdf      )
     if (allocated( duidxj_ff )) deallocate( duidxj_ff )
     if (allocated( tauij ))     deallocate( tauij )
 
