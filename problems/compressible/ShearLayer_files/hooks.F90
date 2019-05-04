e5b00181c1d15dd433562717a997d64f2814933f
Kristen Matsuno <kmatsuno@stanford.edu>
2019-05-04 13:54:16 -0700

    added oscillatory modes

--- problems/compressible/ShearLayer_files/hooks.F90
+++ problems/compressible/ShearLayer_files/hooks.F90
@@ -99,22 +99,20 @@ contains
         deallocate(qhat_real,qhat_imag,qhat)
     end subroutine 
     
-    subroutine make_pert(gp,x,y,z,Lx,Lz,q,maskWidth,maxAmp,nmodes)
+    subroutine make_pert(gp,x,y,z,Lx,Lz,q,maskWidth,maxAmp,nmodes,Mc)
         type(decomp_info), intent(in)               :: gp
         real(rkind), dimension(:,:,:), intent(in)   :: x,y,z ! a 3D grid
         real(rkind), intent(in)                     :: Lx,Lz  ! grid length
         real(rkind), intent(in)                     :: maskWidth,maxAmp 
         integer, intent(in)                         :: nmodes 
+        real(rkind), intent(in)                     :: Mc ! should we use supersonic modes? 
         real(rkind), dimension(:,:,:), intent(inout) :: q !the primitive var
 
-        real(rkind), dimension(gp%ysz(1),gp%ysz(2),gp%ysz(3)) :: mask 
+        real(rkind), dimension(gp%ysz(1),gp%ysz(2),gp%ysz(3)) :: mask,tmp 
         complex(rkind), dimension(gp%ysz(1),gp%ysz(2),gp%ysz(3)) :: e 
         real(rkind) :: kx, kz, k, ph, qmax_local, qmax
-        integer(rkind) :: j, m, mpi_ierr
+        integer(rkind) :: j, m, mx, mz, mpi_ierr
 
-        ! Mask for decaying amplitudes in y
-        mask = maxAmp*exp(-abs(y)/maskWidth)
-       
         ! Add modes to field q
         do m = 3,3+nmodes 
             kx = two*pi/Lx * m
@@ -129,7 +127,31 @@ contains
         ! Get maxval and scale
         qmax_local = maxval(q)
         call mpi_allreduce(qmax_local, qmax, 1, mpirkind, MPI_MAX, MPI_COMM_WORLD)
+        mask = maxAmp*exp(-abs(y)/maskWidth)
         q = q*mask/qmax
+
+        ! We also need more oscillatory modes at higher Mc
+        if (Mc > 0.8) then
+            tmp=0.D0
+            do mx = 3,3+nmodes/2
+            do mz = 3,3+nmodes/2
+                kx = two*pi/Lx * mx
+                kz = two*pi/Lz * mz
+                k  = (kx**two+kz**two)**half
+                call random_number(ph)
+                call mpi_bcast(ph,1,mpirkind,0,MPI_COMM_WORLD,mpi_ierr)
+                e = exp(imi*(kx*x + kz*z)+ph*2*pi)
+                tmp = tmp + real(k**(-5._rkind/3._rkind)*e,rkind) 
+            enddo
+            enddo
+
+            ! Get maxval and scale
+            qmax_local = maxval(tmp)
+            call mpi_allreduce(qmax_local, qmax, 1, mpirkind, MPI_MAX, MPI_COMM_WORLD)
+            mask = 0.5*maxAmp*exp(-abs(y)/maxval(y))*sin(y*2.D0*pi/maskWidth/2)
+            q = q + tmp*mask/qmax
+        endif
+
     end subroutine 
 end module
 
@@ -286,10 +308,11 @@ subroutine initfields(decomp,dx,dy,dz,inputfile,mesh,fields,mix,tsim,tstop,dt,tv
             maskWidth = two*dtheta0
             maxAmp = 1.D-3*du
             nModes = 10 
-            call make_pert(decomp,x,y,z,Lx,Lz,u,maskWidth,maxAmp,nModes)
-            call make_pert(decomp,x,y,z,Lx,Lz,v,maskWidth,maxAmp,nModes)
-            call make_pert(decomp,x,y,z,Lx,Lz,w,maskWidth,maxAmp,nModes)
-            call make_pert(decomp,x,y,z,Lx,Lz,rho,maskWidth,maxAmp,nModes)
+            call make_pert(decomp,x,y,z,Lx,Lz,u,maskWidth,maxAmp,nModes,Mc)
+            call make_pert(decomp,x,y,z,Lx,Lz,v,maskWidth,maxAmp,nModes,Mc)
+            call make_pert(decomp,x,y,z,Lx,Lz,w,maskWidth,maxAmp,nModes,Mc)
+            call make_pert(decomp,x,y,z,Lx,Lz,p,maskWidth,maxAmp,nModes,Mc)
+            call make_pert(decomp,x,y,z,Lx,Lz,rho,maskWidth,maxAmp,nModes,Mc)
         endif
 
         ! Add base flow profiles
