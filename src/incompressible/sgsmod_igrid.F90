c2c55d2fc908ff0fb6a5aa55576f171e15bedb41
Niranjan Ghaisas <ghaisasn@login4.stampede2.tacc.utexas.edu>
2019-07-26 16:39:39 -0500

    Setting up uw_surf and vw_surf in sgsmod

--- src/incompressible/sgsmod_igrid.F90
+++ src/incompressible/sgsmod_igrid.F90
@@ -62,7 +62,7 @@ module sgsmod_igrid
         complex(rkind), dimension(:,:,:), allocatable :: Tfilhat, Tfilhatz1, Tfilhatz2
         logical :: useWallModel = .false.
         integer :: botBC_temp = 1
-        real(rkind), public :: ustar = 1.d0, InvObLength = 0.d0, PsiM = 0.0d0
+        real(rkind), public :: ustar = 1.d0, InvObLength = 0.d0, PsiM = 0.0d0, uw_surf = 0.0d0, vw_surf = 0.0d0
         real(rkind) :: umn = 1.d0, vmn = 1.d0, uspmn = 1.d0, Tmn = 1.d0!, wTh_surf = 0.d0
         real(rkind) :: z0, z0t, meanfact, ThetaRef, Fr, WallMfactor, Re, Pr
         real(rkind), pointer :: Tsurf, wTh_surf
@@ -150,6 +150,8 @@ module sgsmod_igrid
             procedure          :: get_uspeedmean 
             procedure          :: get_DynamicProcedureType
             procedure          :: get_wTh_surf
+            procedure          :: get_uw_surf
+            procedure          :: get_vw_surf
             procedure          :: getMax_DynSmagConst
             procedure          :: getMax_DynPrandtl
             procedure          :: usingDynProc
