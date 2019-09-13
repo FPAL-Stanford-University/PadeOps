62138163be70ed3f01d4d499603169f4806c14dd
Aditya Ghate <aditya90@stanford.edu>
2019-09-13 06:42:04 -0500

    Added channel flow TS-modes? Not sure when this work was done...

---
+++ problems/incompressible/ChannelGrowth_files/temporalHook.F90
@@ -0,0 +1,50 @@
+module temporalHook
+    use kind_parameters,    only: rkind
+    use IncompressibleGrid, only: igrid
+    use reductions,         only: P_MAXVAL, p_minval
+    use exits,              only: message, message_min_max
+    !use EkmanDNS_IO,           only: output_tecplot!dumpData4Matlab 
+    use constants,          only: half
+    use timer,              only: tic, toc 
+    use mpi
+    use decomp_2d
+    use reductions, only: p_sum
+
+    implicit none 
+
+    integer :: nt_print2screen = 1
+    integer :: tid_statsDump = 5000
+    integer :: tid_compStats = 100
+    real(rkind) :: time_startDumping = 10.0_rkind, maxDiv, DomMaxDiv
+    integer :: ierr 
+    
+    integer :: tid_start_planes = 1
+    integer :: tid_stop_planes = 100000
+    integer :: tid_dump_plane_every = 10000
+
+contains
+
+    subroutine doTemporalStuff(igp)
+        class(igrid), intent(inout) :: igp 
+        real(rkind) :: speedTop, um, vm, speedHub
+
+        if (mod(igp%step,nt_print2screen) == 0) then
+            maxDiv = maxval(igp%divergence)
+            DomMaxDiv = p_maxval(maxDiv)
+            call message(0,"Time",igp%tsim)
+            call message(1,"TIDX:",igp%step)
+            call message(1,"MaxDiv:",DomMaxDiv)
+            call message_min_max(1,"Bounds for u:", p_minval(minval(igp%u)),p_maxval(maxval(igp%u)))
+            call message_min_max(1,"Bounds for v:", p_minval(minval(igp%v)),p_maxval(maxval(igp%v)))
+            call message_min_max(1,"Bounds for w:", p_minval(minval(igp%w)),p_maxval(maxval(igp%w)))
+            if (igp%useCFL) then
+                call message(1,"Current dt:",igp%dt)
+            end if
+            call message("==========================================================")
+            call toc()
+            call tic()
+        end if      
+    end subroutine
+
+
+end module 
