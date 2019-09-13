cd20301b4d312a55b2921ef9c85ab16532dfa3f4
Michael Howland <mhowland@login1.stampede2.tacc.utexas.edu>
2019-09-13 17:58:54 -0500

    more dynamic yaw

--- src/incompressible/dynamicYaw.F90
+++ src/incompressible/dynamicYaw.F90
@@ -6,8 +6,8 @@ module dynamicYawMod
     use mpi 
     use reductions, only: p_maxval, p_sum
     use timer, only: tic, toc
-    use Gridtools, only: linspace
-    use sorting_mod, only: Qsort
+    use Gridtools, only: linspace, mytrapz
+    use sorting_mod
     use random,             only: gaussian_random
 
     implicit none
@@ -26,11 +26,11 @@ module dynamicYawMod
         ! Turbine parameters
         integer :: Nt = 1
         real(rkind), dimension(:,:), allocatable :: turbCenter
-        real(rkind) :: diam=0.315d0
-        real(rkind) :: yaw
+        real(rkind) :: D=0.315d0
+        real(rkind), dimension(:), allocatable :: yaw
         ! Wake model parameters
         real(rkind), dimension(:), allocatable :: kw, sigma_0
-        real(rkind) :: powerExp, eps=1.0D-12
+        real(rkind) :: powerExp=2.d0, eps=1.0D-12, Ct, eta
         integer :: Nx
         ! Wind conditions
         real(rkind) :: wind_speed, wind_direction
@@ -38,6 +38,14 @@ module dynamicYawMod
         integer :: conditionTurb
         ! Other stuff
         integer, dimension(:), allocatable :: indSorted, unsort
+        ! Cache stuff
+        real(rkind), dimension(:), allocatable :: u_eff, Cp, a_mom
+        real(rkind), dimension(:), allocatable :: ap, delta_u_face_store
+        real(rkind), dimension(:,:), allocatable :: gaussianStore, y_c, yCenter
+        real(rkind), dimension(:,:), allocatable :: deltaUIndividual, sigmaEnd
+        real(rkind) :: A, rho
+        ! Backward stuff
+        real(rkind), dimension(:), allocatable :: dp_dgamma 
 
     contains
         procedure :: init
@@ -61,10 +69,12 @@ subroutine init(this, inputfile)
     integer :: ioUnit, conditionTurb, ierr
     real(rkind) :: beta1 = 0.9d0, beta2 = 0.999d0, learning_rate_yaw = 10D-4
     integer :: epochsYaw = 5000, stateEstimationEpochs = 500, Ne = 20, Nt = 2
-    real(rkind) :: var_p = 0.04d0, var_k = 9D-6, var_sig = 9D-6
+    real(rkind) :: var_p = 0.04d0, var_k = 9D-6, var_sig = 9D-6, D = 0.315
+    real(rkind) :: Ct = 0.75, eta = 0.7
  
     ! Read input file for this turbine    
-    namelist /DYNAMIC_YAW/ Nt, var_p, var_k, var_sig, epochsYaw, stateEstimationEpochs, Ne
+    namelist /DYNAMIC_YAW/ Nt, var_p, var_k, var_sig, epochsYaw, stateEstimationEpochs, & 
+                           Ne, D, Ct, eta
     ioUnit = 534
     open(unit=ioUnit, file=trim(inputfile), form='FORMATTED', iostat=ierr)
     read(unit=ioUnit, NML=DYNAMIC_YAW)
@@ -74,13 +84,30 @@ subroutine init(this, inputfile)
     this%epochsYaw = epochsYaw; this%stateEstimationEpochs = stateEstimationEpochs;
     this%Ne = Ne
     this%conditionTurb = 1
+    this%Nx = 100; ! for spatial integration
+    this%D = D
+    this%Ct = Ct
+    this%eta = eta
     
     ! Allocate
+    allocate(this%yaw(this%Nt))
+    allocate(this%dp_dgamma(this%Nt))
     allocate(this%indSorted(this%Nt))
+    allocate(this%Cp(this%Nt))
+    allocate(this%a_mom(this%Nt))
     allocate(this%unsort(this%Nt))
     allocate(this%powerObservation(this%Nt))
+    allocate(this%u_eff(this%Nt))
+    allocate(this%sigmaEnd(this%Nt, this%Nt))
+    allocate(this%ap(this%Nt))
+    allocate(this%delta_u_face_store(this%Nt))
+    allocate(this%gaussianStore(this%Nt,this%Nt))
+    allocate(this%y_c(this%Nt,this%Nt))
+    allocate(this%yCenter(this%Nt,this%Nt))
 
- 
+    ! Define
+    this%yaw = 0.d0
+
 end subroutine 
 
 subroutine destroy(this)
@@ -138,7 +165,7 @@ subroutine onlineUpdate(this, X)
     do t=1, this%stateEstimationEpochs;
         call this%EnKF_update( psi_kp1, this%powerObservation, kw, sigma_0)
         ! Run forward model pass
-        call this%forward(kw, sigma_0, Phat)
+        call this%forward(kw, sigma_0, Phat, this%yaw)
         ! Normalized
         Phat = Phat / Phat(1)
         error(t) = p_sum(abs(Phat-this%powerObservation)/Phat(1)) / real(this%Nt)
@@ -201,7 +228,7 @@ subroutine EnKF_update(this, psi_k, P_kp1, kw, sigma_0)
     ! Intermediate forcast step
     do i=1,this%Ne
         ! Lifting line model
-        call this%forward(randArr(:,i), randArr2(:,i), Phat)
+        call this%forward(randArr(:,i), randArr2(:,i), Phat, this%yaw)
         ! Normalize by first turbine
         psiHat_kp(:,i) = Phat/Phat(1)
     end do
@@ -223,17 +250,9 @@ subroutine EnKF_update(this, psi_k, P_kp1, kw, sigma_0)
     
     
     ! Measurement analysis step
-    !psi_kp1 = psi_kp + matmul(matmul(matmul(psi_kpp, transpose(psiHat_kpp)), & 
-    !        inv(matmul(psiHat_kpp,transpose(psiHat_kpp)) + &
-    !        matmul(randArr, transpose(randArr)))), (randArr2 - psiHat_kp))
-    !!!!!!!!!
-    ! Fake one just to compile, removes inverse!!!
-    !!!!!!!!    
     psi_kp1 = psi_kp + matmul(matmul(matmul(psi_kpp, transpose(psiHat_kpp)), & 
-            matmul(psiHat_kpp,transpose(psiHat_kpp)) + &
-            matmul(randArr, transpose(randArr))), (randArr2 - psiHat_kp))
-
-
+            inv(matmul(psiHat_kpp,transpose(psiHat_kpp)) + &
+            matmul(randArr, transpose(randArr)))), (randArr2 - psiHat_kp))
 
     ! Ouput the final values
     psi_kp1 = psi_kp1*ones_Ne;
@@ -246,6 +265,67 @@ end subroutine
 
 subroutine yawOptimize(this)
     class(dynamicYaw), intent(inout) :: this
+
+    ! Model
+    ! Load model inputs
+    [ P, cache, ~ ] = lifting_line_forward_dynamic(turbine, atm, params);
+    Ptot_baseline = sum(P); P_baseline = P;
+    
+    % eps also determines the termination condition
+    k=1; check = false; 
+    m=zeros(turbine.Nt,1); v=zeros(turbine.Nt,1);
+    bestPower = 0; bestYaw = zeros(turbine.Nt,1);
+    Ptot = zeros(params_opti.epochsYaw,1); 
+    P_time = zeros(params_opti.epochsYaw, turbine.Nt);
+    yawTime = zeros(params_opti.epochsYaw, turbine.Nt);
+    while k < params_opti.epochsYaw && check == false;
+    
+        % Forward prop
+        [ P, cache, ~ ] = lifting_line_forward_dynamic(turbine, atm, params);
+        yawTime(k+1,:) = turbine.yaw;
+        Ptot(k) = sum(P);
+        P_time(k,:) = P;
+        yawOld = turbine.yaw;
+        if isfield(cache, 'Cp')==0;
+            k=k+1;
+            break
+        end
+        [ grads ] = lifting_line_backward_dynamic(turbine, atm, params, cache);
+    
+        % Gradient ascent update yaw
+        if strcmp(params_opti.optimizer, 'vanilla') == true;
+            % Vanilla
+            turbine.yaw = turbine.yaw +
+    params_opti.learning_rate_yaw*grads.dp_dgamma;
+        elseif strcmp(params_opti.optimizer, 'adam') == true;
+            % Adam
+            m = params_opti.beta1*m + (1-params_opti.beta1)*grads.dp_dgamma;
+            v = params_opti.beta2*v + (1-params_opti.beta2)*(grads.dp_dgamma.^2);
+            turbine.yaw = turbine.yaw + params_opti.learning_rate_yaw .* m ./
+    (sqrt(v) + params.eps);
+        end
+        if mod(k, params_opti.printInteger) == 0;
+           Ptot(k)/Ptot_baseline 
+           k
+        end
+        if Ptot(k) > bestPower;
+           bestPower = Ptot(k);
+           bestYaw = turbine.yaw;
+        end
+        k = k+1;
+        if k > 10;
+            if abs(Ptot(k-1)-Ptot(k-2))/abs(Ptot(k-1)) < 10^-9;%params.eps;
+                check = true;
+            end
+            if isnan(Ptot(k-1))==1 || Ptot_baseline==0;
+               check=true; 
+            end
+        end
+    end
+    
+    % Final results
+    yaws = bestYaw * 180/pi;
+    P_opti = P;
 
 end subroutine
 
@@ -262,11 +342,7 @@ subroutine rotate(this, X)
     real(rkind), dimension(2,2) ::  R
     real(rkind), dimension(:,:), intent(inout) :: X
     integer, dimension(:), allocatable :: indSorted
-    type group
-        integer :: order    ! original order of unsorted data
-        real :: value       ! values to be sorted by
-    end type group
-    type(group), dimension(1) :: x_for_sorting
+    type(sortgroup), dimension(1) :: x_for_sorting
     integer :: i
     real(rkind) :: a
  
@@ -275,28 +351,243 @@ subroutine rotate(this, X)
     do i=1,this%Nt
         X(i,:) = matmul(this%turbCenter(i,:), transpose(R))
         x_for_sorting(i)%value = X(i,1)
-        x_for_sorting(i)%order = i
+        x_for_sorting(i)%zpos = i
         this%unsort(i) = i
     end do
 
     ! Sort the turbines by upwind location
     call Qsort(x_for_sorting,1)
-    this%indSorted = x_for_sorting%order 
+    this%indSorted = x_for_sorting%zpos
     this%unsort(indSorted) = this%unsort      
     X = X(this%indSorted,:)
 
 end subroutine
 
-subroutine forward(this, kw, sigma_0, Phat)
+subroutine forward(this, kw, sigma_0, Phat, yaw)
     class(dynamicYaw), intent(inout) :: this
-    real(rkind), dimension(:), intent(in) :: kw, sigma_0
-    real(rkind), dimension(:), intent(out) :: Phat
+    real(rkind), dimension(:), intent(in) :: kw, sigma_0, yaw
+    real(rkind), dimension(this%Nt), intent(out) :: Phat
+    real(rkind) :: D, R, dx, boundLow, boundHigh, edgeLow, edgeHigh
+    integer :: Nx, i, j, k
+    real(rkind), dimension(this%Nt) :: delta_v0, a_mom 
+    real(rkind), dimension(this%Nt) :: delta_u0
+    real(rkind), dimension(this%Nt) :: duCache, du_no_a_cache
+    real(rkind), dimension(this%Nt) :: duSum
+    real(rkind), dimension(this%Nt) :: dCt_dyaw
+    real(rkind) :: dw, aPrev, du, gaussian, delta_u_face
+    logical, dimension(this%Nt, this%Nt) :: turbinesInFront
+    real(rkind), dimension(this%Nx) :: xpFront, delta_v
+    logical :: check
+    
+    ! Definitions
+    turbinesInFront = 0
+    this%A = this%D ** 2. * pi / 4
+    D = this%D / this%D
+    R = D/2.
+    ! Atmospheric conditions
+    this%rho = 1.225
+    ! Model parameters
+
+    !! Forward prop
+    ! Loop over turbines
+    do j = 1,this%Nt
+        ! Find the leading turbine
+        do i = 1,this%Nt
+            dx = this%turbCenter(j, 1) - this%turbCenter(i, 1);
+            if (dx > 0) then
+               dw = 1.d0 + kw(i) * log(1.d0 + exp((dx - 2.0d0 * R) / R));
+               boundLow = this%turbCenter(i,2)-dw/2;
+               boundHigh = this%turbCenter(i,2)+dw/2;
+               edgeLow = this%turbCenter(j,2)-this%D/2;
+               edgeHigh = this%turbCenter(j,2)+this%D/2;
+               check = 0
+               if (edgeLow>=boundLow .and. edgeLow<=boundHigh) then
+                   check = 1
+               end if
+               if (edgeHigh>=boundLow .and. edgeHigh<=boundHigh) then
+                   check = 1
+               end if
+               if (check == 1) then
+                   turbinesInFront(j,i) = 1
+               end if
+            end if
+        end do
+        ! sum of squared deficits, loop over front turbines
+        delta_u_face = 0.d0
+        do k = 1, this%Nt
+            if (turbinesInFront(j,k) == 1) then
+                ! sum of squared deficits
+                aPrev = 0.5*(1.d0-sqrt(1.d0-this%Ct*cos(this%yaw(k))**2))
+                dx = this%turbCenter(j, 1) - this%turbCenter(k, 1)
+                xpFront = linspace(0.d0, dx, Nx)
+                dw = 1.d0 + kw(k) * log(1.d0 + exp((dx - 2.0 * R) / R));
+
+                du = this%wind_speed * aPrev/(dw**2+this%eps) * & 
+                        (1.d0+erf(dx/(R*sqrt(2.d0))));
+                this%deltaUIndividual(j,k) = du
+
+                ! Deflection
+                delta_v0(j) = 0.25 * this%Ct * cos(yaw(k))**2 * sin(yaw(k))
+                ! Velocity deficit
+                delta_v = (delta_v0(j) / (dw**2+this%eps)) * 0.5 * (1.d0 + erf(xpFront & 
+                          / (R*sqrt(2.d0))))
+                where (xpFront<0.d0)
+                    delta_v = 0
+                end where
+                ! y_c
+                this%y_c(k, j) = mytrapz(xpFront, -delta_v)
+                ! Local y frame
+                this%sigmaEnd(k, j) = sigma_0(k) * dw 
+                ! take the last value of sigma which is at the next turbine
+                this%yCenter(k, j) = this%y_c(k, j) + this%turbCenter(k, 2);
+                ! Effective velocity at turbine face
+                gaussian = erf((this%turbCenter(j, 2)+D/2.d0-this%yCenter(k,j))/ & 
+                             sqrt(2.d0*this%sigmaEnd(k, j)**2)) - &
+                             erf((this%turbCenter(j, 2)-D/2.d0-this%yCenter(k, j))/ & 
+                             sqrt(2.d0*this%sigmaEnd(k,j)**2));
+                gaussian = gaussian * (D**2)/(16.d0*sigma_0(k)**2) & 
+                           * this%sigmaEnd(k,j)*sqrt(2.d0*pi)
+                delta_u_face = delta_u_face + du * gaussian;
+                this%gaussianStore(k, j) = gaussian;
+            end if
+            this%delta_u_face_store(j) = delta_u_face;
+            this%u_eff(j) = (1.d0/D) * (D*this%wind_speed - delta_u_face);
+        end do
+        ! Calculate the induction factor at the end
+        this%a_mom(j) = 0.5*(1.d0-sqrt(1.d0-this%Ct*cos(yaw(j))**2))
+        ! Power
+        this%ap(j) = 0.5*(1-sqrt(1-this%Ct))
+        this%Cp(j) = 4.d0*this%eta*this%ap(j)*(1.d0-this%ap(j))**2 * & 
+                       cos(yaw(j))**this%powerExp
+        Phat(j) = 0.5 * this%rho * this%A * this%Cp(j) * this%u_eff(j)**3;  
+    end do
 
 end subroutine
 
-subroutine backward(this)
-    class(dynamicYaw), intent(inout) :: this
 
+subroutine backward(this, kw, sigma_0, yaw)
+    class(dynamicYaw), intent(inout) :: this
+    real(rkind), dimension(:), intent(in) :: kw, sigma_0, yaw
+    integer :: i, j, k
+    real(rkind), dimension(this%Nt) :: dp_du_eff, dp_dcp, dcp_dap
+    real(rkind), dimension(this%Nt) :: du_eff_dyc, dy_c_dgamma
+    real(rkind), dimension(this%Nt) :: dcp_dgamma, da_dgamma, xpFront
+    real(rkind) :: dx, R=0.5
+    logical :: check
+    real(rkind) :: boundLow, boundHigh, edgeLow, edgeHigh, dw, du_eff_da
+    logical, dimension(this%Nt) :: turbinesInBack
+
+    !! Backprop
+    turbinesInBack = 0
+    this%dp_dgamma = 0.d0
+    ! Loop over turbines
+    do i = this%Nt, 1, -1
+        ! Same for all turbines
+        ! d a / d gamma
+        da_dgamma(i) = -0.5*this%Ct*sin(yaw(i))*cos(yaw(i))/ & 
+                       sqrt(1.d0-this%Ct*cos(yaw(i))**2 + this%eps)
+        ! dP / d u_eff
+        dp_du_eff(i) = 0.5*this%rho*this%A*this%Cp(i)*this%u_eff(i)**2 * 3.d0;
+        ! dP / dcp
+        dp_dcp(i) = 0.5*this%rho*this%A*this%u_eff(i)**3
+        ! d cp / da
+        dcp_dap(i) = 4.d0*this%eta*(1.d0 - 4.d0*this%ap(i) + &
+                     3.d0*this%ap(i)**2) * cos(yaw(i))**2
+        dcp_dgamma(i) = -4.d0*this%eta * this%ap(i) * (1.d0-this%ap(i))**2*sin(yaw(i)) * &
+                         cos(yaw(i))**(this%powerExp-1.d0) * this%powerExp
+        this%dp_dgamma(i) = this%dp_dgamma(i) + dp_dcp(i)*dcp_dgamma(i);
+    end do
+    do i = this%Nt, 1, -1
+        do j = 1, this%Nt
+            dx = this%turbCenter(j, 1) - this%turbCenter(i, 1);
+            if (dx > 0) then
+               dw = 1.d0 + kw(i) * log(1.d0 + exp((dx - 2.0 * R) / R));
+               boundLow = this%turbCenter(i,2)-dw/2.d0;
+               boundHigh = this%turbCenter(i,2)+dw/2.d0;
+               edgeLow = this%turbCenter(j,2)-this%D/2.d0;
+               edgeHigh = this%turbCenter(j,2)+this%D/2.d0;
+               check = 0
+               if (edgeLow>=boundLow .and. edgeLow<=boundHigh) then
+                   check = 1
+               end if
+               if (edgeHigh>=boundLow .and. edgeHigh<=boundHigh) then
+                   check = 1
+               end if
+               if (check == 1) then
+                   turbinesInBack(i) = 1
+               end if
+            end if
+        end do
+        ! dP / dgamma
+        ! Flip over whether the turbine sees a wake or not
+        du_eff_da = 0;
+        do k = 1, this%Nt
+            if (turbinesInBack(k) == 1) then
+                du_eff_da = -this%deltaUIndividual(k,i) / (this%a_mom(i)+this%eps) * & 
+                            this%gaussianStore(i,k)
+                this%dp_dgamma(i) = this%dp_dgamma(i) + & 
+                                    dp_du_eff(k)*du_eff_da*da_dgamma(i)
+                ! d u_eff / d y_c
+                du_eff_dyc(i) = -(1.d0/this%D) * (this%delta_u_face_store(k) * & 
+                                this%D**2 / (8.d0*sigma_0(i)**2) * &
+                                (-exp(-(this%turbCenter(k, 2)+this%D/2.d0-&
+                                this%yCenter(i, k))**2/(2.d0*this%sigmaEnd(i,k)**2))+& 
+                                exp(-(this%turbCenter(k, 2)-this%D/2.d0-&
+                                this%yCenter(i, k))**2/(2.d0*this%sigmaEnd(i,k)**2))) )
+                !!!
+                ! d y_c / d gamma
+                dx = this%turbCenter(k, 1) - this%turbCenter(i, 1)
+                xpFront = linspace(0.d0, dx, this%Nx)
+                dw = 1.d0 + kw(i) * log(1.d0 + exp((dx - 2.0d0 * R) / R))
+                ! Changed this when using the local velocity to compute y_c
+                dy_c_dgamma(i) = (cos(yaw(i))**3 - 2.d0*sin(yaw(i))**2 * & 
+                                 cos(yaw(i))) * &
+                                 mytrapz(xpFront, -0.25d0*this%Ct*0.5d0*(1.d0+erf(xpFront/ & 
+                                 (R*sqrt(2.d0))))/dw**2)
+                ! Sum together to get dP / dgamma
+                this%dp_dgamma(i) = this%dp_dgamma(i) + & 
+                                    dp_du_eff(k)*du_eff_dyc(i)*dy_c_dgamma(i) 
+            end if
+        end do    
+    end do
+    
 end subroutine
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!! LAPACK
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+function inv(A) result(Ainv)
+  real(rkind), dimension(:,:), intent(in) :: A
+  real(rkind), dimension(size(A,1),size(A,2)) :: Ainv
+
+  real(rkind), dimension(size(A,1)) :: work  ! work array for LAPACK
+  integer, dimension(size(A,1)) :: ipiv   ! pivot indices
+  integer :: n, info
+
+  ! External procedures defined in LAPACK
+  external DGETRF
+  external DGETRI
+
+  ! Store A in Ainv to prevent it from being overwritten by LAPACK
+  Ainv = A
+  n = size(A,1)
+
+  ! DGETRF computes an LU factorization of a general M-by-N matrix A
+  ! using partial pivoting with row interchanges.
+  call DGETRF(n, n, Ainv, n, ipiv, info)
+
+  if (info /= 0) then
+     stop 'Matrix is numerically singular!'
+  end if
+
+  ! DGETRI computes the inverse of a matrix using the LU factorization
+  ! computed by DGETRF.
+  call DGETRI(n, Ainv, n, ipiv, work, n, info)
+
+  if (info /= 0) then
+     stop 'Matrix inversion failed!'
+  end if
+end function inv
 
 end module 
