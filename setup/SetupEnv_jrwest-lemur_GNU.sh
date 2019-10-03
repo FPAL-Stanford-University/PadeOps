2a2ef7eb93f588543ceb4403722fcb016721c6f4
Jacob <jrwest@stanford.edu>
2019-10-03 16:57:22 -0700

    new setup file for my personal computer (incomplete

---
+++ setup/SetupEnv_jrwest-lemur_GNU.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export COMPILER_ID=GNU
+export FC=mpif90.openmpi
+export F77=mpif90.openmpii
+export CC=mpicc.openmpi
+export MPICC=mpicc.openmpi
+export CXX=mpicxx.openmpi
+export FFTW_PATH=/opt/fftw-3.3.4-GNU
+export DECOMP_PATH=/opt/2decomp_fft-GNU
+export VTK_IO_PATH=/home/akshays/Codes/PadeOps/dependencies/Lib_VTK_IO_GNU/build
