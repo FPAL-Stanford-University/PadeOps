31b6c430f013fdb36211f9f638b21f8d1a3b48e5
Niranjan Ghaisas <nghaisas@nsws.caelab.mae.iith.ac.in>
2021-03-22 21:54:14 +0530

    Getting PadeOps to compile on nsws AMD machine

---
+++ setup/SetupEnv_IITHnsws_GNU.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export COMPILER_ID=GNU
+export FC=mpifort
+export CC=mpicc
+export CXX=mpicxx
+export FFTW_PATH=/clhome/shared/depgnu485/fftw-3.3.5
+export DECOMP_PATH=/clhome/shared/depgnu485/2decomp_fft
+export VTK_IO_PATH=/clhome/shared/depgnu485/Lib_VTK_IO/build
+export HDF5_PATH=/clhome/shared/depgnu485/hdf5-1.8.18
