c2bd026ae512de7a39de5b20693bf448766e0170
Zoe Barbeau <zbarbeau@sh02-01n58.int>
2021-06-30 18:40:29 -0700

    add surface tension	../problems/solids/Droplet_Shear/

--- src/solid/SolidMixtureEOS.F90
+++ src/solid/SolidMixtureEOS.F90
@@ -2235,7 +2235,7 @@ stop
  
 
         !TODO: Use this%surfaceTension_f to compute this%surfaceTension_e
-	this%surfaceTension_e = u*this%surfaceTension_f(:,:,:,1) +v*this%surfaceTension_f(:,:,:,2) +w*this%surfaceTension_f(:,:,:,2) 
+	this%surfaceTension_e = u*this%surfaceTension_f(:,:,:,1) +v*this%surfaceTension_f(:,:,:,2) +w*this%surfaceTension_f(:,:,:,3) 
 
     end subroutine get_surfaceTension
 
