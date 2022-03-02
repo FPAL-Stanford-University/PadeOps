e41843b6617e9d138d95708f0c39e94722e0678f
Niranjan <nghaisas@cn1.hpc715>
2022-03-02 15:33:35 +0530

    Allocating space for 2nd derivatives for inviscid problems as well

--- src/incompressible/igrid.F90
+++ src/incompressible/igrid.F90
@@ -691,14 +691,14 @@ contains
        allocate(this%rbuffyE(this%gpE%ysz(1),this%gpE%ysz(2),this%gpE%ysz(3),2))
        allocate(this%rbuffzE(this%gpE%zsz(1),this%gpE%zsz(2),this%gpE%zsz(3),4))
        allocate(this%filteredSpeedSq(this%gpC%xsz(1),this%gpC%xsz(2),this%gpC%xsz(3)))
-       if (.not.this%isinviscid) then
+       !if (.not.this%isinviscid) then
            allocate(this%d2udz2hatC(this%sp_gpC%ysz(1),this%sp_gpC%ysz(2),this%sp_gpC%ysz(3)))
            allocate(this%d2vdz2hatC(this%sp_gpC%ysz(1),this%sp_gpC%ysz(2),this%sp_gpC%ysz(3)))
            allocate(this%d2wdz2hatE(this%sp_gpE%ysz(1),this%sp_gpE%ysz(2),this%sp_gpE%ysz(3)))
            if (this%isStratified) then
               allocate(this%d2Tdz2hatC(this%sp_gpC%ysz(1),this%sp_gpC%ysz(2),this%sp_gpC%ysz(3)))
            end if 
-       end if 
+       !end if 
        this%nxZ = size(this%cbuffzE,1); this%nyZ = size(this%cbuffzE,2)
        allocate(this%fbody_x(this%gpC%xsz(1), this%gpC%xsz(2), this%gpC%xsz(3)))
        allocate(this%fbody_y(this%gpC%xsz(1), this%gpC%xsz(2), this%gpC%xsz(3)))
