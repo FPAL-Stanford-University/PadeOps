a4bea12009ba1d4e85ebed5ba46f3922f85070c4
Niranjan Ghaisas <nghaisas@iith.ac.in>
2022-06-02 10:18:58 +0530

    Adding/deleting dependency sourcing files

---
+++ setup/SetupEnv_IITHSahyadri_GNU_LocalDependencies.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export COMPILER_ID=GNU
+export FC=mpifort
+export CC=mpicc
+export CXX=mpicxx
+export FFTW_PATH=/home/nghaisas/DepTemp/fftw-3.3.5
+export DECOMP_PATH=/home/nghaisas/DepTemp/2decomp_fft
+export VTK_IO_PATH=/home/nghaisas/DepTemp/Lib_VTK_IO/build
+export HDF5_PATH=/home/nghaisas/DepTemp/hdf5-1.8.18
---
+++ setup/SetupEnv_IITHSahyadri_GNU_LocalDependencies2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export COMPILER_ID=GNU
+export FC=mpifort
+export CC=mpicc
+export CXX=mpicxx
+export FFTW_PATH=/home/nghaisas/Codes/PadeOps/dependencies/fftw-3.3.5
+export DECOMP_PATH=/home/nghaisas/Codes/PadeOps/dependencies/2decomp_fft
+export VTK_IO_PATH=/home/nghaisas/Codes/PadeOps/dependencies/Lib_VTK_IO/build
+export HDF5_PATH=/home/nghaisas/Codes/PadeOps/dependencies/hdf5-1.8.18
