b8b1e6a6ad3064d2bf94e751c763da6f0c8da07e
Andy Wu <xsedeu3108@r0login0.localdomain.hpc.udel.edu>
2023-03-06 16:37:09 -0500

    first commit

---
+++ setup/SetupEnv_Darwin_Intel.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+vpkg_devrequire intel
+vpkg_devrequire intel-oneapi
+vpkg_devrequire git
+vpkg_devrequire cmake
+vpkg_devrequire openmpi
+CWD=/lustre/xg-phy230025/users/3108/PadeOps/PadeOps
+export COMPILER_ID=Intel
+export FC=mpiifort
+export CC=mpiicc
+export CXX=mpiicpc
+export FFTW_PATH=${CWD}/dependencies/fftw-3.3.5
+export DECOMP_PATH=${CWD}/dependencies/2decomp_fft
+export VTK_IO_PATH=${CWD}/dependencies/Lib_VTK_IO/build
+export HDF5_PATH=${CWD}/dependencies/hdf5-1.8.18
+export FFTPACK_PATH=${CWD}/dependencies/fftpack
+export ARCH_OPT_FLAG="-xCORE-AVX2 -axMIC-AVX512"
