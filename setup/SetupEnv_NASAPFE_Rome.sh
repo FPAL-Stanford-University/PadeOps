c3b40a17581a31cec2df62d71958d0149f4176d9
Ryan Hass <rdhass@pfe27.nas.nasa.gov>
2023-03-15 09:34:56 -0700

    modified NASA PFE setup file

--- setup/SetupEnv_NASAPFE_Rome.sh
+++ setup/SetupEnv_NASAPFE_Rome.sh
@@ -5,9 +5,9 @@ export COMPILER_ID=Intel
 export FC=mpif90 
 export CC="icc -lmpi"
 export CXX="icc -lmpi"
-export FFTW_PATH=/nobackup/aghate/PadeOps/dependencies/fftw-3.3.10
-export DECOMP_PATH=/nobackup/aghate/PadeOps/dependencies/2decomp_fft
-export VTK_IO_PATH=/nobackup/aghate/PadeOps/dependencies/Lib_VTK_IO/build
-export HDF5_PATH=/nobackup/aghate/PadeOps/dependencies/hdf5-1.8.18
-export FFTPACK_PATH=/nobackup/aghate/PadeOps/dependencies/fftpack
+export FFTW_PATH=$CWD/dependencies/fftw-3.3.10
+export DECOMP_PATH=$CWD/dependencies/2decomp_fft
+export VTK_IO_PATH=$CWD/dependencies/Lib_VTK_IO/build
+export HDF5_PATH=$CWD/dependencies/hdf5-1.8.18
+export FFTPACK_PATH=$CWD/dependencies/fftpack
 export ARCH_OPT_FLAG="-axCORE-AVX2 -qopt-zmm-usage=high"
