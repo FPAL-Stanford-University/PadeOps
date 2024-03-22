b0f47fae7f67842963e98a208e282c1f458773ed
naveen.mae.iith <me20resch11010@iith.ac.in>
2024-03-22 11:58:43 +0530

    adding paramseva setup file

---
+++ setup/SetupEnv_Paramseva.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export COMPILER_ID=Intel
+export FC=mpiifort
+export CC=mpiicc
+export CXX=mpiicpc
+export FFTW_PATH=/home/naveen.mae.iith/codes/PadeOps/dependencies/fftw-3.3.5
+export DECOMP_PATH=/home/naveen.mae.iith/codes/PadeOps/dependencies/2decomp_fft
+export VTK_IO_PATH=/home/naveen.mae.iith/codes/PadeOps/dependencies/Lib_VTK_IO/build
+export HDF5_PATH=/home/naveen.mae.iith/codes/PadeOps/dependencies/hdf5-1.8.18
