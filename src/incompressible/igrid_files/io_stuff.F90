b8bbb44e9931fda8f125b52f52a6bdf70f79dbb5
Ryan Hass <rdhass@gmail.com>
2025-04-27 14:48:16 -0500

    Fixed file names for potential vorticity planes

--- src/incompressible/igrid_files/io_stuff.F90
+++ src/incompressible/igrid_files/io_stuff.F90
@@ -466,7 +466,7 @@
                    call decomp_2d_write_plane(1,this%oz,dirid, pid, fname, this%gpC)
                    if (this%isStratified) then ! Compute potential vorticity
                        call this%compute_potential_vorticity(this%rbuffxC(:,:,:,1))
-                       write(tempname,"(A3,I2.2,A2,I6.6,A2,I5.5,A8)") "Run", this%RunID,"_t",&
+                       write(tempname,"(A3,I2.2,A2,I6.6,A2,I5.5,A4)") "Run", this%RunID,"_t",&
                          tid,"_y",pid,".pPV"
                        fname = this%OutputDir(:len_trim(this%OutputDir))//"/"//trim(tempname)
                        call decomp_2d_write_plane(1,this%rbuffxC(:,:,:,1),dirid, pid, fname, this%gpC)
